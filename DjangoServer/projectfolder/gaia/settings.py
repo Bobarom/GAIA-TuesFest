69cf7e2fe4a53ca9a63178225658790ca4e4b732
Bobarom <bobarkumanov@gmail.com>
2026-04-11 16:29:43 +0300

    connected django to the databse

--- DjangoServer/projectfolder/gaia/settings.py
+++ DjangoServer/projectfolder/gaia/settings.py
@@ -74,8 +74,10 @@ WSGI_APPLICATION = 'gaia.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        "OPTIONS": {
+            "service": "my_service",
+        },
     }
 }
 
@@ -104,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Sofia'
 
 USE_I18N = True
 
